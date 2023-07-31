ce64d6af6aa51fa2f8d54c074b283b44eb8f7112
osoniee <113271057+osoniee@users.noreply.github.com>
2023-07-31 13:41:05 +0900

    remove useless comment

--- spring-aspectj/src/main/kotlin/com/example/aop/spring/aspectj/aspect/LogAspect.kt
+++ spring-aspectj/src/main/kotlin/com/example/aop/spring/aspectj/aspect/LogAspect.kt
@@ -10,16 +10,6 @@ open class LogAspect {
     @Pointcut("@annotation(logged)")
     fun loggedPointCut(logged: Logged?) {}
 
-//    @Throws(Throwable::class)
-//    @Around("loggedPointCut(logged)")
-//    fun logAround(joinPoint: ProceedingJoinPoint, logged: Logged): Any? {
-//        if (!logged.logAround) return joinPoint.proceed()
-//        println("_____________around start____________")
-//        val returnValue: Any? = joinPoint.proceed()
-//        println("_____________around end______________")
-//        return returnValue
-//    }
-
     @Before("loggedPointCut(logged)")
     open fun logBefore(logged: Logged) {
         if (!logged.logBefore) return
